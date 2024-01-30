6521de8ceb971afc286a744d16e1c038c6d3ba61
arontha <tharpy04@hotmail.com>
2024-01-30 13:37:34 -0500

    linting

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -1,38 +1,35 @@
+import pandas
+import requests
 import streamlit
+import snowflake.connector
+from urllib.error import URLError
 
+#header
 streamlit.title('Snowflake Badge 2 Title')
-
 streamlit.header('Snowflake Badge 2 Header')
-
 streamlit.text('Snowflake Badge 2 Text')
 
 
-import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
 streamlit.dataframe(fruits_to_show)
-
 streamlit.header("Fruityvice Fruit Advice!")
 
-import requests
 
+#first data 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
-
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
 #normalize json api response
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #display api response
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
 
+#Snowflake
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
