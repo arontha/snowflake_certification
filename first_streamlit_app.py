967314b32863c7d86018320e8c34ccc2e6a0adf4
arontha <tharpy04@hotmail.com>
2024-01-30 11:23:06 -0500

    cleanup fruityvice api response to display

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -20,4 +20,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+
+#normalize json api response
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#display api response
+streamlit.dataframe(fruityvice_normalized)
